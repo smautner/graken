8dff3b352f4b5bd521c6d573132991113e6552d4
S Mautner <mautner@cs.uni-freiburg.de>
2021-05-04 08:31:58 +0000

    runall improved

--- graken/runall.fish
+++ graken/runall.fish
@@ -1,9 +1,13 @@
 source setvar.fish 
 
-set i  default greedy paretogreed pareto_only # random 
 set stuff  -j 32 --bar --results LOL  -j 32 --bar --results LOL python main.py   
-set prog --shuffle {3} --i tasks/task_{2} --n_train 500 --n_iter 3 --out res/{1}_{2}_{3} --pareto {1}
-set args ::: $i ::: (seq 0 99) ::: (seq 0 5)
+
+set static --n_train 500 --n_iter 3 --cipselector 1 --cipselector_k 66
+set prog --shuffle {3} --i tasks/task_{2} --out res/{1}_{2}_{3} --pareto {1}
+
+# ARGS
+set pareto default greedy paretogreed pareto_only 
+set argvalues ::: $pareto ::: (seq 0 99) ::: (seq 0 5)
 
 
-parallel $stuff $prog $args
+parallel $stuff $prog $static $argvalues
